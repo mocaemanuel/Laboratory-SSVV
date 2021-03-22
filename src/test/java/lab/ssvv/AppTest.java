9240a9be839d2b7b633c4dbbd2de00d0d0358c34
mocamanuel <mocaemanuel@yahoo.com>
2021-03-22 19:30:19 +0200

    More tests

--- src/test/java/lab/ssvv/AppTest.java
+++ src/test/java/lab/ssvv/AppTest.java
@@ -42,12 +42,44 @@ public class AppTest {
     }
 
     @Test
-    public void testAddCorrectStudent() {
+    public void testAddStudentWithCorrectUpperBoundGroup() {
         assertEquals(0, service.saveStudent("1", "test", 935));
     }
 
     @Test
-    public void testAddIncorrectStudent() {
+    public void testAddStudentWithInorrectUpperBoundGroup() {
         assertEquals(1, service.saveStudent("1", "bun", 940));
+    }
+
+    @Test
+    public void testAddStudentWithCorrectLowerBoundGroup() {
+        assertEquals(0, service.saveStudent("1", "bun", 120));
+    }
+
+    @Test
+    public void testAddStudentWithInorrectLowerBoundGroup() {
+        assertEquals(1, service.saveStudent("1", "bun", 105));
+    }
+
+    @Test
+    public void testAddStudentWithCorrectName() {
+        assertEquals(0, service.saveStudent("1", "nume bun", 120));
+    }
+
+    @Test
+    public void testAddStudentWithIncorrectName() {
+        assertEquals(1, service.saveStudent("1", "", 105));
+        assertEquals(1, service.saveStudent("1", null, 105));
+    }
+
+    @Test
+    public void testAddStudentWithCorrectID() {
+        assertEquals(0, service.saveStudent("1", "nume", 120));
+    }
+
+    @Test
+    public void testAddStudentWithIncorrectID() {
+        assertEquals(1, service.saveStudent("", "nume", 105));
+        assertEquals(1, service.saveStudent(null, "nume", 105));
     }
 }
